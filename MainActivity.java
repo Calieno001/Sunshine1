02d4da512572c94877e8755e6981611768bc14c1
Calieno001 <calieno001@gmail.com>
2016-05-16 23:09:10 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.samson.sunshine.app;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    public static class PlaceholderFragment extends Fragment{
+//        public PlaceholderFragment(){
+//
+//        }
+//
+//        public View onCreateView(LayoutInflater inflater, ViewGroup)
+//    }
+}
